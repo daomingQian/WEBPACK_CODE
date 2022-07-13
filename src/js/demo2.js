342403294b1bd5d55d928e68a7461727aba19e1d
daomingQian <813928419@qq.com>
2022-07-13 23:42:41 +0800

    学习webpack第一天 配置webpack

---
+++ src/js/demo2.js
@@ -0,0 +1,3 @@
+export default function reduce(a,b){
+    return a*b;
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import add from './js/demo1'
+import reduce from './js/demo2'
+console.log(add(10,15));
+console.log(reduce(100,12));
