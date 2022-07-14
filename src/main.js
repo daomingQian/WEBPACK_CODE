c015930656eb8a73a9331feb280b849736d56885
daomingQian <813928419@qq.com>
2022-07-14 20:55:45 +0800

    第三次提交less，scss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './css/index.css'
+import './less/index.less'
+import './sass/index.scss'
 import add from './js/demo1'
 import reduce from './js/demo2'
 console.log(add(10,15));
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,26 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                }, {
+                    loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                }, {
+                    loader: "sass-loader" // 将 Sass 编译成 CSS
+                }]
             }
         ]
     },
