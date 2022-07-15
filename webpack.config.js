9860db57e96a5594a48e28445c61a842461fdfeb
daomingQian <813928419@qq.com>
2022-07-15 21:13:04 +0800

    change of congig

--- webpack.config.js
+++ webpack.config.js
@@ -14,23 +14,11 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "less-loader" // compiles Less to CSS
-                }]
+                use: ["style-loader" , "css-loader" ,  "less-loader"]
             },
             {
                 test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // 将 JS 字符串生成为 style 节点
-                }, {
-                    loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-                }, {
-                    loader: "sass-loader" // 将 Sass 编译成 CSS
-                }]
+                use: ["style-loader","css-loader","sass-loader"]
             },
             {
                 test: /\.(png|jpe?g|gif|webp|svg)$/,
